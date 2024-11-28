8695a06d748c98329f076ded0b9fbe0b479a5229
Samarth <programminglovers97@gmail.com>
2024-11-28 21:00:35 +0530

    o

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <div className = "mainDiv">
-      <span>Ram Ram</span>
-      <span>Working...</span>
+      <div className="flex flex-col">
+        <span>Ram Ram</span>
+        <span>Working...</span>
+      </div>
     </div>
     
   );
